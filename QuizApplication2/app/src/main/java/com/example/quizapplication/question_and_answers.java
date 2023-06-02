dfe7780da49e1c0b73327f303e94f8680312e0f9
kunal pandey <kp7834810@gmail.com>
2023-06-02 09:45:40 +0530

    Quiz Application in Android Added

---
+++ QuizApplication2/app/src/main/java/com/example/quizapplication/question_and_answers.java
@@ -0,0 +1,25 @@
+package com.example.quizapplication;
+
+public class question_and_answers {
+
+    public static String question[]={
+
+            "Which Company Owns the android ?",
+            "Which one is not the programming language ?",
+            "Where you can watch video,can do live stream and create your own channel ?"
+    };
+
+    public static String choices[][]={
+
+            {"Google","Apple","Nokia","SamSung"},
+            {"Java","Notepad","C++","Kotlin"},
+            {"Facebook","Instagram","Whatsapp","YouTube"}
+    };
+
+    public static String correct_answers[]={
+      "Google",
+      "Notepad",
+      "YouTube"
+    };
+
+}
